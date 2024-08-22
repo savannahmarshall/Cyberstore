34839fb37bcacbdb3974993f815102cba07bdc8c
savannahmarshall <savvymarshall@gmail.com>
2024-08-22 10:24:41 -0700

    defined columns in category.js and associations in index.js

--- models/index.js
+++ models/index.js
@@ -4,13 +4,28 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Products belong to a Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
 
-// Products belongToMany Tags (through ProductTag)
+// Products belong to many Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+});
 
-// Tags belongToMany Products (through ProductTag)
+// Tags belong to many Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
+});
 
 module.exports = {
   Product,
